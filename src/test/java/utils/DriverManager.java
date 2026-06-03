16a5bc8a742d6e40a22ad421fb09fca01c285b85
sastimaulii <sastimaulii17@gmail.com>
2026-06-04 01:10:20 +0700

    fix yml and web driver

--- src/test/java/utils/DriverManager.java
+++ src/test/java/utils/DriverManager.java
@@ -1,19 +1,28 @@
 package utils;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverManager {
 
     private static WebDriver driver;
 
     public static WebDriver getDriver() {
+
         if (driver == null) {
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
-            driver.manage().window().maximize();
+
+            ChromeOptions options = new ChromeOptions();
+
+            // 🔥 HEADLESS MODE (untuk CI + optional lokal)
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--window-size=1920,1080");
+
+            driver = new ChromeDriver(options);
         }
+
         return driver;
     }
 
